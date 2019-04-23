e3c480b8ae9afc7754efa3da1336607e9c0ac783
Rufino Rojas <lucras015@hotmail.com>
2019-04-23 18:37:41 -0500

    se agrego funcionalidad para el componente de egresos

--- src/app/egresos/egresos.component.ts
+++ src/app/egresos/egresos.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Egreso } from './egresos.model';
+import { EgresoServicio } from './egresos.service';
 
 @Component({
   selector: 'app-egresos',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EgresosComponent implements OnInit {
 
-  constructor() { }
+  egresos: Egreso[]=[];
+  @Input() ingresoTotal:number;
+
+  constructor(private egresosServicio: EgresoServicio) { }
 
   ngOnInit() {
+    this.egresos = this.egresosServicio.egresos;
+  }
+
+  eliminarEgreso(egreso:Egreso){
+    this.egresosServicio.eliminar(egreso);
+  }
+
+  calcularPorcentaje(egreso:Egreso){
+    return egreso.valor / this.ingresoTotal;
   }
 
 }
--- src/app/egresos/egresos.service.ts
+++ src/app/egresos/egresos.service.ts
@@ -3,5 +3,12 @@ export class EgresoServicio{
   egresos:Egreso[] = [
     new Egreso("Renta",400),
     new Egreso("Ropa", 400)
-  ]
+  ];
+
+
+  eliminar(egreso:Egreso){
+    const indice: number = this.egresos.indexOf(egreso);
+    this.egresos.splice(indice, 1);
+
+  }
 }
