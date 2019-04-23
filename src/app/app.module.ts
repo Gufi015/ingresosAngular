4dc26eaaa31d859569fb02445a37efb5872d5892
Rufino Rojas <lucras015@hotmail.com>
2019-04-23 16:55:58 -0500

    se agregaron componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PresupuestoComponent } from './presupuesto/presupuesto.component';
+import { IngresosComponent } from './ingresos/ingresos.component';
+import { EgresosComponent } from './egresos/egresos.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PresupuestoComponent,
+    IngresosComponent,
+    EgresosComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
